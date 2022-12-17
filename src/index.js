2498842621f50bb927e762131d55021f1d4fe57a
Davhho-dev <Davhho@utexas.edu>
2022-12-17 11:35:38 -0600

    Add Menu list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { navBar } from "./home";
 import { mainContent } from "./home";
+import menu from "./menu";
 import "./style.css";
 
 const content = document.getElementById("content");
+// content.appendChild(menu());
+
+
+
+
 content.appendChild(navBar());
 content.appendChild(mainContent());
+
