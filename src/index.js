e48468b4760155fc49d57671823452a06e519dc1
Davhho-dev <Davhho@utexas.edu>
2022-12-17 19:18:18 -0600

    Create click event for tabs and remove current page and update with new page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import menu from "./menu";
 import "./style.css";
 
 const content = document.getElementById("content");
+// content.appendChild(navBar());
 // content.appendChild(menu());
-
-
-
 
 content.appendChild(navBar());
 content.appendChild(mainContent());
 
+const links = document.querySelectorAll("a");
+links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        if(e.target.textContent === "home") {
+            content.removeChild(content.childNodes[1]);
+            content.appendChild(mainContent());
+        }
+        else if(e.target.textContent === "menu") {
+            content.removeChild(content.childNodes[1]);
+            content.appendChild(menu());
+        }
+    })
+})
